619583d17ec6aa73d42f3eb729af2aeb3c8adbca
Phuctran11 <trancaobaophuc2019@gmai.com>
2024-12-06 07:18:54 +0700

    update make span schedule lab 5

--- THUAT TOAN UNG DUNG/LAB BUOI 5/make_span_schedule.cpp
+++ THUAT TOAN UNG DUNG/LAB BUOI 5/make_span_schedule.cpp
@@ -1,3 +1,35 @@
+// Description
+// A project has n tasks 1,. . ., n. Task i has duration d(i) to be completed (i=1,. . ., n). There are precedence constraints between tasks represented by a set Q of pairs: for each (i,j)  in Q, task j cannot be started before the completion of task i. Compute the earliest completion time  of the project.
+// Input
+// Line 1: contains n and m (1 <= n <= 10
+// 4
+// , 1 <= m <= 200000)
+// Line 2: contains d(1),. . ., d(n) (1 <= d(i) <= 1000)
+// Line i+3 (i=1,. . ., m) : contains i and j : task j cannot be started to execute before the completion of task i
+// Output
+// Write the earliest completion time of the project.
+// Example
+// Input
+// 9 13
+// 5 3 1 2 6 4 3 1 4
+// 1 3
+// 1 5
+// 1 6
+// 2 1
+// 2 3
+// 3 5
+// 4 1
+// 4 2
+// 4 6
+// 5 8
+// 7 9
+// 9 5
+// 9 8
+
+// Output
+// 18
+
+
 #include <iostream>
 #include <vector>
 #include <queue>
